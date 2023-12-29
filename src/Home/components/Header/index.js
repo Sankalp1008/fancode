35d253eec77eaef52af18986b848107e4c9d9a48
SankalpRoots <sankalp.chaturvedi@justmyroots.com>
2023-12-29 12:43:33 +0530

    first-commit

---
+++ src/Home/components/Header/index.js
@@ -0,0 +1,158 @@
+import React from 'react'
+import logo from '../../../images/fancode-fc.png'
+import { Tab, Tabs, makeStyles ,Box,Typography} from '@material-ui/core'
+import PropTypes from 'prop-types'
+const useStyles = makeStyles(theme =>({
+root:{
+ flexGrow:1,
+},
+
+  mainHeader:{
+background:'#242424',
+position:'sticky',
+top:0,
+  },
+
+  logo:{
+    [theme.breakpoints.down('sm')]: {
+      padding : '10.735px 36.827px 1.069px 7px',
+      alignItems:'center',
+    }
+  },
+
+  filters:{
+    '& .MuiTabs-indicator': {
+      display: 'none',
+    },
+    '& .MuiTabs-flexContainer': {
+      // padding:16,
+      display: 'flex',
+      gridTemplateColumns: '1fr 1fr 1fr 1fr',
+      gridColumnGap: 10,
+      [theme.breakpoints.down('sm')]: {
+        gridColumnGap: 10,
+      },
+    },
+  },
+
+  tabTitle:{
+    color:'white',
+    gap:10,
+    fontFamily:'Saira, sans-serif',
+    backgroundColor: '#484848',
+    borderRadius: 4,
+    fontSize: '14px !important',
+    textTransform:'capitalize',
+    fontWeight: 400,
+    minHeight: 34,
+    '&.Mui-selected': {
+      backgroundColor: '#F0283C',
+      color: '#ffffff',
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '8px',
+    },
+  },
+
+  tabSelected:{
+    color:'white',
+    gap:10,
+    fontFamily:'Saira, sans-serif',
+    backgroundColor: '#F0283C',
+    borderRadius: 4,
+    fontSize: '14px !important',
+    textTransform:'capitalize',
+    fontWeight: 400,
+    minHeight: 34,
+    '&.Mui-selected': {
+      backgroundColor: '#F0283C',
+      color: '#ffffff',
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '8px',
+    },
+  },
+
+ 
+
+}))
+
+const TabPanel = ({ children, value, isSelected }) => {
+  return (
+    <div role="tabpanel" hidden={!isSelected}>
+      {/* {isSelected && } */}
+    </div>
+  );
+};
+
+
+const Header = ({value,handleChange=()=>null,filterData=[],genreValue}) => {
+
+  const classes = useStyles()
+  const isTabSelected = (tabValue) => genreValue.includes(tabValue);
+
+  return (
+    
+    <div className={classes.root}> 
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Saira&display=swap');
+</style>
+    <div className={classes.mainHeader}>
+      <div className={classes.logo}>
+      <img src={logo} alt='logo'/>
+      </div>
+      <div >
+          <div style={{padding:'0px 16px 1px 16px'}}>
+                  <Box mt={4} className={classes.filters}>
+
+ <Tabs 
+aria-label="multi-select tabs example"
+ variant='scrollable'
+ onChange={handleChange}
+ value={genreValue}
+
+ >
+ {filterData && filterData.map((item,idx)=>
+
+(
+    <Tab
+    // {...a11yProps(idx)}
+    label={item.name}
+    className={isTabSelected(item.id) ? classes.tabSelected :classes.tabTitle}
+    value={item.id}
+    
+
+    /> 
+  
+)
+)}
+
+ </Tabs>
+ {genreValue.map((selectedTab) => (
+        <TabPanel key={selectedTab} value={selectedTab} isSelected={isTabSelected(selectedTab)}>
+          {/* Content for each selected tab */}
+        </TabPanel>
+      ))}
+
+ </Box>
+
+{/* <div className={classes.tabsWrapper}>
+<TabPanel value={item.id} index={item.id}>
+<Box
+  display="flex"
+  justifyContent="space-between"
+  alignItems="center"
+>
+<Typography>{item.name}</Typography>
+</Box>
+</TabPanel>
+</div> */}
+</div>
+
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Home/index.js
@@ -0,0 +1,165 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Header from './components/Header'
+import axios from 'axios'
+import MovieCards from './components/MovieCards'
+import { Grid, makeStyles } from '@material-ui/core'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+
+const useStyles = makeStyles(theme => ({
+
+  movies: {
+    margin: '0 auto',
+    padding: '2%',
+  },
+  heading: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 600,
+    padding: '2%',
+
+  },
+
+  listings: {
+    marginTop: 10,
+  }
+
+}))
+
+
+const Home = () => {
+  const [filterData, setFilterData] = useState([])
+  const [genreValue, setGenreValue] = useState([])
+  const api_key = '18d6e20cf4a6513047912ef9c6a8455a'
+  const [currentYear, setCurrentYear] = useState(2012)
+  const [movieList, setMovieList] = useState([])
+  const [movieGroups, setMovieGroups] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const classes = useStyles()
+
+  useEffect(() => {
+    getGenres()
+  }, [])
+
+  useEffect(() => {
+    getMovies(currentYear)
+  }, [genreValue])
+
+  const handleChange = (e, newValue) => {
+    const index = genreValue.indexOf(newValue)
+    if (index === -1) {
+      setMovieGroups([])
+      setCurrentYear(2012)
+      setGenreValue((prevSelectedTabs) => [...prevSelectedTabs, newValue])
+
+    } else {
+      setGenreValue((prevSelectedTabs) => prevSelectedTabs.filter((tab) => tab !== newValue));
+
+    }
+  }
+
+  const getGenres = () => {
+    axios.get('https://api.themoviedb.org/3/genre/movie/list?language=en', {
+      params: { api_key }
+    })
+      .then((res) => {
+        setFilterData(res.data.genres)
+
+      })
+  }
+
+  const getMovies = (year) => {
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=2dca580c2a14b55200e784d157207b4d&sort_by=popularity.desc&primary_release_year=${year}&page=1&vote_count.gte=100&with_genres=${genreValue}`, {
+
+    })
+      .then((res) => {
+        setMovieList(res.data.results)
+        const newMovies = res.data.results
+        const groupedMovies = newMovies.reduce((acc, movie) => {
+          const releaseYear = movie.release_date ? movie.release_date.split('-')[0] : 'Unknown'
+          if (!acc[releaseYear]) {
+            acc[releaseYear] = [];
+          }
+
+          acc[releaseYear].push(movie);
+          return acc;
+        }, {})
+        setMovieGroups((prevGroups) => [...prevGroups, groupedMovies]);
+
+
+      })
+  }
+
+  const fetchNextYear = () => {
+    if (currentYear < 2023) {
+
+      setCurrentYear((prevYear) => prevYear + 1);
+      getMovies(currentYear + 1)
+    } else {
+      setHasMore(false)
+    }
+  }
+
+  const fetchPreviousYear = () => {
+    if (currentYear > 2012) {
+      setCurrentYear((prevYear) => prevYear - 1);
+      getMovies(currentYear - 1);
+    }
+  }
+
+
+
+  const findGenreById = (gId, dataArray) => {
+    return gId.map((id) => {
+      const item = dataArray.find((item) => item.id === id)
+      return item ? item.name : 'not found'
+    })
+
+  }
+
+
+
+
+  return (
+    <div>
+      <Header
+        filterData={filterData}
+        handleChange={handleChange}
+        genreValue={genreValue}
+      />
+      <div className={classes.movies} >
+        <InfiniteScroll
+          dataLength={movieGroups.length}
+          next={fetchNextYear}
+          hasMore={hasMore}
+
+        >
+          {movieGroups.map((group, index) => (
+            <div className={classes.listings} >
+              <div className={classes.heading}>{Object.keys(group)[0]}</div>
+              <Grid container spacing={0}>
+                {group[Object.keys(group)[0]].map((movie) => (
+                  <Grid key={movie.id} item xs={6} >
+                    <MovieCards
+                      image={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                      title={movie.title}
+                      rating={movie.vote_average}
+                      genre={findGenreById(movie.genre_ids, filterData).join(', ')}
+                      desc={movie.overview}
+                    />
+                  
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
+          ))}
+
+        </InfiniteScroll>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
