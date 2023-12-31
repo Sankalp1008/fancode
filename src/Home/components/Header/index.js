83b06bd8161951c1ac0899be2556a7a929dd6cc2
SankalpRoots <sankalp.chaturvedi@justmyroots.com>
2023-12-31 19:04:08 +0530

    more bugs2

--- src/Home/components/Header/index.js
+++ src/Home/components/Header/index.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import logo from '../../../images/fancode-fc.png'
-import { Tab, Tabs, makeStyles ,Box,Typography} from '@material-ui/core'
-import PropTypes from 'prop-types'
-const useStyles = makeStyles(theme =>({
-root:{
- flexGrow:1,
-},
-
-  mainHeader:{
-background:'#242424',
-position:'sticky',
-zIndex: 100,
-left: 0,
-
-top:0,
+import { Tab, Tabs, makeStyles, Box } from '@material-ui/core'
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
   },
 
-  logo:{
+  mainHeader: {
+    background: '#242424',
+    position: 'sticky',
+    zIndex: 100,
+    left: 0,
+
+    top: 0,
+  },
+
+  logo: {
     [theme.breakpoints.down('sm')]: {
-      padding : '10.735px 36.827px 1.069px 7px',
-      alignItems:'center',
+      padding: '10.735px 36.827px 1.069px 7px',
+      alignItems: 'center',
     }
   },
 
-  filters:{
+  filters: {
     '& .MuiTabs-indicator': {
       display: 'none',
     },
@@ -38,14 +37,14 @@ top:0,
     },
   },
 
-  tabTitle:{
-    color:'white',
-    gap:10,
-    fontFamily:'Saira, sans-serif',
+  tabTitle: {
+    color: 'white',
+    gap: 10,
+    fontFamily: 'Saira, sans-serif',
     backgroundColor: '#484848',
     borderRadius: 4,
     fontSize: '14px !important',
-    textTransform:'capitalize',
+    textTransform: 'capitalize',
     fontWeight: 400,
     minHeight: 34,
     '&.Mui-selected': {
@@ -57,14 +56,14 @@ top:0,
     },
   },
 
-  tabSelected:{
-    color:'white',
-    gap:10,
-    fontFamily:'Saira, sans-serif',
+  tabSelected: {
+    color: 'white',
+    gap: 10,
+    fontFamily: 'Saira, sans-serif',
     backgroundColor: '#F0283C',
     borderRadius: 4,
     fontSize: '14px !important',
-    textTransform:'capitalize',
+    textTransform: 'capitalize',
     fontWeight: 400,
     minHeight: 34,
     '&.Mui-selected': {
@@ -76,84 +75,70 @@ top:0,
     },
   },
 
- 
+
 
 }))
 
-const TabPanel = ({ children, value, isSelected }) => {
+const TabPanel = ({ isSelected }) => {
   return (
     <div role="tabpanel" hidden={!isSelected}>
-      {/* {isSelected && } */}
     </div>
   );
 };
 
 
-const Header = ({value,handleChange=()=>null,filterData=[],genreValue}) => {
+const Header = ({ value, handleChange = () => null, filterData = [], genreValue }) => {
 
   const classes = useStyles()
   const isTabSelected = (tabValue) => genreValue.includes(tabValue);
 
   return (
-    
-    <div style={{ position: 'sticky', top: 0, zIndex: 999 }}> 
-    <style>
-@import url('https://fonts.googleapis.com/css2?family=Saira&display=swap');
-</style>
-    <div className={classes.mainHeader}>
-      <div className={classes.logo}>
-      <img src={logo} alt='logo'/>
+
+    <div style={{ position: 'sticky', top: 0, zIndex: 999 }}>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Saira&display=swap');
+      </style>
+      <div className={classes.mainHeader}>
+        <div className={classes.logo}>
+          <img src={logo} alt='logo' />
+        </div>
+        <div >
+          <div style={{ padding: '0px 16px 1px 16px' }}>
+            <Box mt={4} className={classes.filters}>
+
+              <Tabs
+                aria-label="multi-select tabs example"
+                variant='scrollable'
+                onChange={handleChange}
+                value={genreValue}
+
+              >
+                {filterData && filterData.map((item, idx) =>
+
+                (
+                  <Tab
+                    label={item.name}
+                    className={isTabSelected(item.id) ? classes.tabSelected : classes.tabTitle}
+                    value={item.id}
+
+
+                  />
+
+                )
+                )}
+
+              </Tabs>
+              {genreValue.map((selectedTab) => (
+                <TabPanel key={selectedTab} value={selectedTab} isSelected={isTabSelected(selectedTab)}>
+                </TabPanel>
+              ))}
+
+            </Box>
+
+          </div>
+
+        </div>
       </div>
-      <div >
-          <div style={{padding:'0px 16px 1px 16px'}}>
-                  <Box mt={4} className={classes.filters}>
-
- <Tabs 
-aria-label="multi-select tabs example"
- variant='scrollable'
- onChange={handleChange}
- value={genreValue}
-
- >
- {filterData && filterData.map((item,idx)=>
-
-(
-    <Tab
-    // {...a11yProps(idx)}
-    label={item.name}
-    className={isTabSelected(item.id) ? classes.tabSelected :classes.tabTitle}
-    value={item.id}
-    
-
-    /> 
-  
-)
-)}
-
- </Tabs>
- {genreValue.map((selectedTab) => (
-        <TabPanel key={selectedTab} value={selectedTab} isSelected={isTabSelected(selectedTab)}>
-          {/* Content for each selected tab */}
-        </TabPanel>
-      ))}
-
- </Box>
-
-{/* <div className={classes.tabsWrapper}>
-<TabPanel value={item.id} index={item.id}>
-<Box
-  display="flex"
-  justifyContent="space-between"
-  alignItems="center"
->
-<Typography>{item.name}</Typography>
-</Box>
-</TabPanel>
-</div> */}
-</div>
-
-      </div>
-    </div>
     </div>
   )
 }
--- src/Home/components/MovieCards/index.js
+++ src/Home/components/MovieCards/index.js
@@ -1,112 +1,110 @@
 import React from 'react'
-import { Tab, Tabs, makeStyles ,Box,Typography} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 
-const useStyles = makeStyles(theme =>({
+const useStyles = makeStyles(theme => ({
 
-mainCard:{
-    [theme.breakpoints.down('sm')]: {
+    mainCard: {
+        [theme.breakpoints.down('sm')]: {
 
-    minWidth:160,
-    margin:10,
-    position:'relative',
-    textAlign:'center',
-    },
-'& img':{
-    [theme.breakpoints.down('sm')]: {
-        width:'100%',
+            minWidth: 160,
+            margin: 10,
+            position: 'relative',
+            textAlign: 'center',
+        },
+        '& img': {
+            [theme.breakpoints.down('sm')]: {
+                width: '100%',
+            }
+        },
+
+        '& .details': {
+            [theme.breakpoints.down('sm')]: {
+
+                position: 'absolute',
+                bottom: -4,
+                // left:4,
+                width: '100%',
+                backgroundImage: 'linear-gradient(#250606c9,#2b0a0a)'
+            },
+            '& .title': {
+                [theme.breakpoints.down('sm')]: {
+                    color: 'white',
+                    fontSize: 10,
+                    fontWeight: 600,
+                    position: 'relative',
+                    left: 4,
+
+                    textAlign: 'left',
+                    width: '90%',
+
+                }
+            },
+            '& .rating': {
+                [theme.breakpoints.down('sm')]: {
+                    color: 'white',
+                    fontSize: 10,
+                    fontWeight: 600,
+                    position: 'relative',
+                    left: 4,
+                    textAlign: 'left',
+                    width: '80%',
+
+                }
+            },
+            '& .genre': {
+                [theme.breakpoints.down('sm')]: {
+                    color: 'white',
+                    fontSize: 10,
+                    fontWeight: 600,
+                    position: 'relative',
+                    left: 4,
+                    textAlign: 'left',
+                    width: '80%',
+                    // textDecoration:'underline',
+
+
+                }
+            },
+            '& .desc': {
+                [theme.breakpoints.down('sm')]: {
+                    color: 'white',
+                    fontSize: 10,
+                    fontWeight: 600,
+                    position: 'relative',
+                    left: 2,
+                    textAlign: 'left',
+                    width: '90%',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+
+                }
+            },
+        },
+
+
+
     }
-},
-
-'& .details':{
-    [theme.breakpoints.down('sm')]: {
-
-    position:'absolute',
-    bottom:-4,
-    // left:4,
-    width:'100%',
-    backgroundImage:'linear-gradient(#250606c9,#2b0a0a)'
-    },
-    '& .title':{
-        [theme.breakpoints.down('sm')]: {
-            color:'white',
-            fontSize:10,
-            fontWeight:600,
-            position:'relative',
-            left:4,
-        
-            textAlign:'left',
-            width:'90%',
-    
-        }
-    },
-    '& .rating':{
-        [theme.breakpoints.down('sm')]: {
-            color:'white',
-            fontSize:10,
-            fontWeight:600,
-            position:'relative',
-            left:4,
-            textAlign:'left',
-            width:'80%',
-    
-        }
-    },
-    '& .genre':{
-        [theme.breakpoints.down('sm')]: {
-            color:'white',
-            fontSize:10,
-            fontWeight:600,
-            position:'relative',
-            left:4,
-            textAlign:'left',
-            width:'80%',
-            // textDecoration:'underline',
-            
-    
-        }
-    },
-    '& .desc':{
-        [theme.breakpoints.down('sm')]: {
-            color:'white',
-            fontSize:10,
-            fontWeight:600,
-            position:'relative',
-            left:2,
-            textAlign:'left',
-            width:'90%',
-            textOverflow:'ellipsis',
-            whiteSpace:'nowrap',
-            overflow:'hidden',
-    
-        }
-    },
-},
-
-
-
-}
 
 }))
 
-const MovieCards = ({image,title,rating,genre,cast,director,desc}) => {
+const MovieCards = ({ image, title, rating, genre, cast, director, desc }) => {
     const classes = useStyles()
-  return (
-    <div className={classes.mainCard}> 
-        <img src={image} alt="img"/>
-        <div className='details'>
-        <div className='title'>{title}</div>
-        <div className='rating'>
-            {rating}
+    return (
+        <div className={classes.mainCard}>
+            <img src={image} alt="img" />
+            <div className='details'>
+                <div className='title'>{title}</div>
+                <div className='rating'>
+                    {rating}
+                </div>
+                <div className='genre'>
+                    {genre}
+                </div>
+
+            </div>
         </div>
-        <div className='genre'>
-            {genre}
-        </div>
-        {/* <div className='desc'>
-            {desc}
-        </div> */}
-        </div>
-    </div>
-  )
+    )
 }
 
 export default MovieCards
